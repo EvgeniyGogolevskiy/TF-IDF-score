fe585a4cc31a67c25aef4c0fb6f296129f5c9e70
Evgeny <evgeni3197@mail.ru>
2025-04-26 20:44:54 +0300

    add pagination, update style, add docker_compose, update models

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, UploadFile, File, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles  # Импортируем для работы с файлами
 from app.services.text_processing import process_text
 import uvicorn
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+# Настройка для отдачи статических файлов
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
+def root():
+    return RedirectResponse(url="/upload")
+
+@app.get("/upload", response_class=HTMLResponse)
 async def upload_form(request: Request):
     return templates.TemplateResponse("upload.html", {"request": request})
 
@@ -15,7 +25,7 @@ async def upload_form(request: Request):
 async def upload_file(request: Request, file: UploadFile = File(...)):
     content = await file.read()
     words_stats = process_text(content.decode('utf-8'))
-    words_stats = sorted(words_stats, key=lambda x: -x['idf'])[:50]
+    words_stats = sorted(words_stats, key=lambda x: -x.idf)[:50]
     return templates.TemplateResponse("result.html", {"request": request, "words": words_stats})
 
 if __name__ == "__main__":
--- app/services/text_processing.py
+++ app/services/text_processing.py
@@ -1,8 +1,9 @@
 import math
 from collections import Counter
+from typing import List
+from app.models import WordStats
 
-
-def process_text(text: str):
+def process_text(text: str) -> List[WordStats]:
     words = [word.lower() for word in text.split() if word.isalpha()]
     tf_counter = Counter(words)
     total_words = len(words)
@@ -11,9 +12,5 @@ def process_text(text: str):
     for word, count in tf_counter.items():
         tf = count
         idf = math.log(total_words / (1 + count))
-        result.append({
-            "word": word,
-            "tf": tf,
-            "idf": round(idf, 5)
-        })
+        result.append(WordStats(word=word, tf=tf, idf=round(idf, 5)))
     return result
